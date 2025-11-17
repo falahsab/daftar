35d26aab227c5784984b8d1da038c22e38e47613
falahsab <40568313+falahsab@users.noreply.github.com>
2025-11-17 19:53:52 +0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -74,7 +74,7 @@ async function loadTransactions(){
 renderTransactions(
     clientTransactions
         .sort((a,b)=> new Date(b.date) - new Date(a.date))
-        .slice(0,3)
+        .slice(0,5)
 );
 }
 function showAllTransactions(){ renderTransactions(clientTransactions); }
